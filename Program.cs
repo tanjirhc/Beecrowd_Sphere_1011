927feadeb207cb5ad0a8bf624616b7c9e1722c0e
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-11 07:54:35 +0600

    = added

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Beecrowd_Sphere_1011
             double Result = ((4 / 3.0) * 3.14159 * R * R * R);
 
 
-            Console.WriteLine("VOLUME: {0:00.000}", Result);
+            Console.WriteLine("VOLUME = {0:00.000}", Result);
             Console.ReadKey();
         }
     }
