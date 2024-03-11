3df7a573ef6b4713f29d0fa266e3e6f17e100562
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-11 07:49:49 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_Sphere_1011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = Convert.ToDouble(Console.ReadLine());
+            double Result = ((4 / 3.0) * 3.14159 * R * R * R);
+
+
+            Console.WriteLine("VOLUME: {0:00.000}", Result);
+            Console.ReadKey();
+        }
+    }
+}
